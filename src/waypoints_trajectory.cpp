19ffb99c7900782c06613dea48081c6c217da595
Aleks <aleks.attanasio@gmail.com>
2021-02-15 14:23:55 +0000

    Added waypoints planning

---
+++ src/waypoints_trajectory.cpp
@@ -0,0 +1,78 @@
+#include <pluginlib/class_loader.h>
+#include <ros/ros.h>
+
+// MoveIt
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_interface/planning_interface.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/kinematic_constraints/utils.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/PlanningScene.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+#include <boost/scoped_ptr.hpp>
+
+int main(int argc, char** argv) {
+
+    const std::string node_name = "motion_planning_tutorial";
+    ros::init(argc, argv, node_name);
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+    ros::NodeHandle node_handle("~");
+
+    // Create a RobotState and JointModelGroup to keep track of the current robot pose and planning group
+    const std::string PLANNING_GROUP = "psm_arm";
+    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+    robot_model::RobotModelPtr robot_model = robot_model_loader.getModel();
+    robot_state::RobotStatePtr robot_state(new robot_state::RobotState(robot_model));
+    const robot_state::JointModelGroup *joint_model_group = robot_state->getJointModelGroup(PLANNING_GROUP);
+
+    planning_scene::PlanningScenePtr planning_scene(new planning_scene::PlanningScene(robot_model));
+    planning_scene->getCurrentStateNonConst().setToDefaultValues(joint_model_group, "ready");
+
+    // Load plugin planner
+    boost::scoped_ptr<pluginlib::ClassLoader<planning_interface::PlannerManager>> planner_plugin_loader;
+    planning_interface::PlannerManagerPtr planner_instance;
+    std::string planner_plugin_name;
+
+    // Handle plugin boot
+    if (!node_handle.getParam("/move_group/planning_plugin", planner_plugin_name))
+        ROS_FATAL_STREAM("Could not find planner plugin name");
+    try {
+        planner_plugin_loader.reset(new pluginlib::ClassLoader<planning_interface::PlannerManager>(
+                "moveit_core", "planning_interface::PlannerManager"));
+    }
+    catch (pluginlib::PluginlibException &ex) {
+        ROS_FATAL_STREAM("Exception while creating planning plugin loader " << ex.what());
+    }
+    try {
+        planner_instance.reset(planner_plugin_loader->createUnmanagedInstance(planner_plugin_name));
+        if (!planner_instance->initialize(robot_model, node_handle.getNamespace()))
+            ROS_FATAL_STREAM("Could not initialize planner instance");
+        ROS_INFO_STREAM("Using planning interface '" << planner_instance->getDescription() << "'");
+    }
+    catch (pluginlib::PluginlibException &ex) {
+        const std::vector<std::string> &classes = planner_plugin_loader->getDeclaredClasses();
+        std::stringstream ss;
+        for (std::size_t i = 0; i < classes.size(); ++i)
+            ss << classes[i] << " ";
+        ROS_ERROR_STREAM("Exception while loading planner '" << planner_plugin_name << "': " << ex.what() << std::endl
+                                                             << "Available plugins: " << ss.str());
+    }
+
+    // Setup of the rviz scene
+    namespace rvt = rviz_visual_tools;
+    moveit_visual_tools::MoveItVisualTools visual_tools("world");
+    visual_tools.loadRobotStatePub("/display_robot_state");
+    visual_tools.enableBatchPublishing();
+    visual_tools.deleteAllMarkers();  // clear all old markers
+    visual_tools.trigger();
+
+    visual_tools.loadRemoteControl();
+
+    // Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations
+    visual_tools.trigger();
+    visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
+
+    std::vector<geometry_msgs::PoseStamped>
+}
