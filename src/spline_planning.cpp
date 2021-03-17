198d8250b4621affa371ecc7a58ac626dc27e053
Aleks <aleks.attanasio@gmail.com>
2021-03-17 16:29:58 +0000

    First draft of trajectory JS-CC conversion method

--- src/spline_planning.cpp
+++ src/spline_planning.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <dvrk_moveit_class.h>
 #include <string.h>
+#include <moveit_msgs/GetPositionFK.h>
 
 // MoveIt!
 #include <moveit/planning_interface/planning_interface.h>
@@ -25,7 +26,7 @@ int main(int argc, char** argv) {
     mid.setupPlanningScene();
 
     // ### DEFINE WAYPOINTS ###
-    mid.waypoints = mid.getWaypointsVector('L');
+    mid.waypoints = mid.getWaypointsVector('B');
     mid.checkPoseValidity(mid.home_pose);
     mid.checkWaypointsValidity(mid.waypoints);
 
@@ -41,6 +42,36 @@ int main(int argc, char** argv) {
     // ### DEFINE GOAL POSE AND COMPILE MOTION PLAN REQUEST ###
     moveit_msgs::Constraints pose_goal_end = mid.computeGoalConstraint(mid.waypoints.at(2));
     mid.compileMotionPlanRequest(pose_goal_end, trajectory);
+
+
+    for (int i = 1; i<trajectory.joint_trajectory.points.size(); i++) {
+        std::vector<double> joint_values;
+
+        for (int k = 0; k<trajectory.joint_trajectory.joint_names.size(); k++) {
+            joint_values.push_back(trajectory.joint_trajectory.points[i].positions[k]);}
+
+        //initialize joint values
+        mid.robot_state->setJointGroupPositions(mid.joint_model_group->getName(), joint_values);
+        const Eigen::Affine3d &link_pose = mid.robot_state->getGlobalLinkTransform("psm_tool_tip_link");
+        Eigen::Vector3d cartesian_position = link_pose.translation();
+        Eigen::Matrix3d link_orientation = link_pose.rotation();
+//        Eigen::Quaternionf rot_quat(link_orientation);
+
+        // populate Pose Message
+        geometry_msgs::Pose tp;
+        tp.position.x = cartesian_position.x();
+        tp.position.y = cartesian_position.y();
+        tp.position.z = cartesian_position.z();
+//        tp.orientation.w = rot_quat.w();
+//        tp.orientation.x = rot_quat.x();
+//        tp.orientation.y = rot_quat.y();
+//        tp.orientation.z = rot_quat.z();
+
+        std::cout << "x: " << tp.position.x << std::endl;
+        std::cout << "y: " << tp.position.y << std::endl;
+        std::cout << "z: " << tp.position.z << std::endl;
+    }
+
 
     // SOLVE REQUEST
     planning_interface::PlanningContextPtr context = planner_instance->getPlanningContext(mid.planning_scene, mid.req, mid.res.error_code_);
